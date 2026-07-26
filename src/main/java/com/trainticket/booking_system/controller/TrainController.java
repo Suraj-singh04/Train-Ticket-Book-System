a63153e49173b1b11c0d834baf3e6d7bd8287700
Suraj-singh04 <surajsingh877028@gmail.com>
2026-07-27 02:48:31 +0530

    feat(train): complete CRUD REST API for train management

--- src/main/java/com/trainticket/booking_system/controller/TrainController.java
+++ src/main/java/com/trainticket/booking_system/controller/TrainController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +39,26 @@ public class TrainController {
         return ResponseEntity.ok(
             trainService.getAllTrains()
         );
+    }
+
+    @GetMapping("/{trainId}")
+    public ResponseEntity<TrainResponse> getTrainById(@PathVariable String trainId) {
+        TrainResponse response = trainService.getTrainById(trainId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{trainId}")
+    public ResponseEntity<TrainResponse> updateTrain(@PathVariable String trainId, @RequestBody CreateTrainRequest request) {
+        TrainResponse response = trainService.updateTrain(trainId, request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{trainId}")
+    public ResponseEntity<Void> deleteTrain(@PathVariable String trainId) {
+        trainService.deleteTrain(trainId);
+
+        return ResponseEntity.noContent().build();
     }
 }
